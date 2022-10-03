05661e118e84c81053663a9fa946303baa5bc695
Evgeniy_Vladimir <6484588@mail.ru>
2022-10-03 22:36:06 +0300

    PyHW2

--- PyHW/PyHW2/PyHW2_tsk3.py
+++ PyHW/PyHW2/PyHW2_tsk3.py
@@ -1,1 +1,9 @@
-# 3.Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+# 3.Задайте список из N чисел последовательности (1+1/N)^N и выведите на экран их сумму.
+N = int(input('Введите число: '))
+ans = list()
+
+sum = 0
+for i in range(1, N+1):
+    sum = int(sum + round((1+1/i)**i, 2))
+    ans.append(sum)
+print(f'if N = {N}, then ansver is: {list(enumerate(ans, start = 1))}')
