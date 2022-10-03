30a284578fbf44f805c614bd5ba125fe56782d60
Evgeniy_Vladimir <6484588@mail.ru>
2022-10-04 00:23:52 +0300

    PyHW6

--- PyHW/PyHW6/PyHW6_tsk4.py
+++ PyHW/PyHW6/PyHW6_tsk4.py
@@ -1,8 +1,6 @@
 #Задача2 Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-
 # _____________
 # Было
 # n = int(input('Введите число: '))
@@ -14,9 +12,14 @@
 # print(a)  
 # _________________
 # стало
-def factorial(numb):
-    if numb == 1:
+def factorial(n):
+    if n == 1:
         return 1
     else:
-        return numb * factorial(numb - 1)
-print(*list(map(factorial, [i for i in range(1, int(input('Введите число: ')) + 1)])))
+        return n * factorial(n-1)
+print(list(map(factorial, [i for i in range(1, int(input('Введите число: '))+1)])))
+
+# import math
+# x = int(input(' Введите число: '))                     # \
+# factorial = lambda x: x and x * factorial(x-1) or 1    #  > не работает
+# print(factorial(x))                                    # /
